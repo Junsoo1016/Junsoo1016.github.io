4c999ee16426c856c9037d778a64deb2b1c17384
Jun <junsoopark92@gmail.com>
2022-04-28 16:32:18 -0400

    first draft

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const toggleBtn = document.querySelector('.navbar__toogleBtn');
 const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
+const cancelBtn = document.querySelector('.navbar__cancelBtn')
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
-    icons.classList.toggle('active');
 });
 
+const changeIcon = () => {
+    const a = document.getElementById('.navbar__toogleBtn');
+    a.classList.toggle('.navbar__cancelBtn');
+}
