aeffbb9914d8efbff0d71698182e58d953144ad7
Jun <junsoopark92@gmail.com>
2022-04-28 22:20:51 -0400

    "update website"

--- main.js
+++ main.js
@@ -7,7 +7,24 @@ toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
+new TypeIt('#first', {
+    startDelay: 400,
+    strings: 'Junsoo Park',
+    afterComplete: () => {
+      new TypeIt('#second', {
+        startDelay: 400,
+        strings: "[ 'HTML', 'CSS', 'JavaScript' ]", 
+          
+
+
+        }).go();
+    }
+  }).go();
+  
+
 const changeIcon = () => {
     const a = document.getElementById('.navbar__toogleBtn');
     a.classList.toggle('.navbar__cancelBtn');
 }
+
+
